3f4112c930eb9fad4287c7019b9491f43635af3c
hadrian3689 <71423134+hadrian3689@users.noreply.github.com>
2025-07-05 13:07:31 -0400

    Fixed indent func_xor.cpp

--- func_xor.cpp
+++ func_xor.cpp
@@ -3,14 +3,13 @@
 int main(){
     char shellcode_payload[] = "CreateProcessW";
 
-
-char key[]="mysecretkey";
-int j=0;
-for (int i = 0; i < sizeof(shellcode_payload); i++){  
-    if(j==sizeof(key)-1){
-          j=0;
-    }
-    printf("0x%02x, ",shellcode_payload[i]^key[j]);
-          j++;
+    char key[]="mysecretkey";
+    int j=0;
+    for (int i = 0; i < sizeof(shellcode_payload); i++){  
+            if(j==sizeof(key)-1){
+                    j=0;
+            }
+            printf("0x%02x, ",shellcode_payload[i]^key[j]);
+            j++;
     }
 }
